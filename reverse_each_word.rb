cef361707db7542e61263303023e0b2aff1472fa
Alex Rodriguez <alexrod102797@gmail.com>
2019-08-13 00:43:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,11 +7,11 @@ def reverse_each_word(string)
   new = new.join(" ") 
   new 
 end
-
-def reverse_each_word
+def reverse_each_word(string)
+  
   string = string.split(" ")
     string.collect do |x|
     x.reverse
-    string.join(" ")
-  end
     end
+  string.join(" ")
+end
